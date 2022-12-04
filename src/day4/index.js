0bea73aa2e7dd9e93aa29eae8488e0d97d88f740
Rachel Sanham <green_chul@hotmail.com>
2022-12-04 03:01:51 +0000

    added day 4 solution

---
+++ src/day4/index.js
@@ -0,0 +1,11 @@
+const whisper = (string) => {
+  const result = `shh... ${
+    string.toLowerCase().endsWith("!")
+      ? string.toLowerCase().slice(0, string.length - 1)
+      : string.toLowerCase()
+  }`;
+  return result;
+};
+
+console.log(whisper("PLEASE STOP SHOUTING."));
+console.log(whisper("MA'AM, this is a Wendy's!"));
